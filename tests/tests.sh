065c4814bdce1a63a0659b6e3841542d73a9d09b
Daniel <daniel@binaryparadox.net>
2017-06-24 16:14:58 -0400

    Refactor/Cleanup test/test.sh
    This commit updates `tests/tests.sh` to have less duplication & to pass
    a shellcheck invocation. The commonality between it and the base
    `streisand` script was extracted to `util/ansible_check.sh` and included
    in both places. To make shellcheck follow the includes we add a `-x` to
    the `.travis.yml` invocation of shellcheck.

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,27 +1,74 @@
 #!/usr/bin/env bash
-# Streisand syntax check.
+# Streisand CI test script.
+# Usage:
+#  ./tests.sh [setup|syntax|run|ci|full]
+
+# Set errexit option to exit immediately on any non-zero status return
 set -e
 
+echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  S T R E I S A N D  \033[0m\n"
+
+# Compute an absolute path to the test ansible.cfg file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export ANSIBLE_CONFIG=$DIR/ansible.cfg
 
-command -v ansible > /dev/null 2>&1 || {
-  echo "Ansible is not installed."
-  exit 1
+# Include the check_ansible function from ansible_check.sh
+source util/ansible_check.sh
+
+function run_playbook {
+  PLAYBOOK="$1"
+  EXTRA_FLAGS=(${@:2})
+  ansible-playbook -i "$DIR/inventory" "$PLAYBOOK" "${EXTRA_FLAGS[@]}"
 }
 
-required_ansible_version="2.3.0.0"
+# syntax_check runs `ansible-playbook` with `--syntax-check` to vet Ansible
+# playbooks for syntax errors
+function syntax_check {
+  run_playbook "$DIR/syntax-check.yml" --syntax-check -vv
+}
 
-if [[ "$(ansible --version | grep -oe '2\(.[0-9]\)*')" < $required_ansible_version ]]
-then
-    echo "Ansible $required_ansible_version or higher is required."
-    exit 1
-fi
+function dev_setup {
+  run_playbook "$DIR/development-setup.yml"
+}
 
-if [ -z "$*" ]; then ansible-playbook -i $DIR/inventory $DIR/syntax-check.yml --syntax-check -vv ; fi
+function run_tests {
+  run_playbook "$DIR/run.yml" -e streisand_ci=false "$1"
+}
 
-if [[ $1 = "ci" ]]; then  ansible-playbook -i $DIR/inventory $DIR/development-setup.yml && ansible-playbook -i $DIR/inventory $DIR/run.yml -e streisand_ci=true; fi
-if [[ $1 = "full" ]]; then  ansible-playbook -i $DIR/inventory $DIR/development-setup.yml -vv && ansible-playbook -i $DIR/inventory $DIR/run.yml -vv -e streisand_ci=false; fi
-if [[ $1 = "run" ]]; then  ansible-playbook -i $DIR/inventory $DIR/run.yml -e streisand_ci=false -vv; fi
-if [[ $1 = "setup" ]]; then  ansible-playbook -i $DIR/inventory $DIR/development-setup.yml -vv; fi
-if [[ $1 = "syntax" ]]; then ansible-playbook -i $DIR/inventory $DIR/syntax-check.yml --syntax-check -vv ; fi
+function ci_tests {
+  dev_setup && run_tests
+}
+
+function ci_tests_verbose {
+  dev_setup && run_tests -vv
+}
+
+# Make sure the system is ready for the Streisand playbooks
+check_ansible
+
+case $1 in
+  # Setup only prepares the local environment for running a Streisand LXC
+  "setup")
+    dev_setup
+    ;;
+  # Syntax only checks for Ansible syntax errors
+  "syntax")
+    syntax_check
+    ;;
+  # Run will run CI tests assuming the local environment is already prepared
+  "run")
+    run_tests
+    ;;
+  # CI will setup the local environment and then run tests
+  "ci")
+    ci_tests
+    ;;
+  # Full will do the same as "ci" but with verbose output
+  "full")
+    ci_tests_verbose
+    ;;
+  # By default, just run a syntax_check
+  *)
+    syntax_check
+    ;;
+esac
