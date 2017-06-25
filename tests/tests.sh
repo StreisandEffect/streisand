8e94442422eb73d43c588879c365cb3f8c5171de
Daniel <daniel@binaryparadox.net>
2017-06-25 11:46:36 -0400

    Reenable streisand_ci flag to fix build.
    Somehow I had set `streisand_ci` to false when it should obviously be
    true when running CI tests!

--- tests/tests.sh
+++ tests/tests.sh
@@ -32,7 +32,7 @@ function dev_setup {
 }
 
 function run_tests {
-  run_playbook "$DIR/run.yml" -e streisand_ci=false "$1"
+  run_playbook "$DIR/run.yml" -e streisand_ci=true "$1"
 }
 
 function ci_tests {
