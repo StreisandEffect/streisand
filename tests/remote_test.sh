b784e3beff0bc07f23e3d424c7a8cd3d398a88aa
Phil Porada <pgporada@users.noreply.github.com>
2017-07-05 22:25:48 -0400

    Run shellcheck against shell scripts (#785)
    * Ran shellcheck against shell scripts
    
    * Added shellcheck for remote test script to travis.

--- tests/remote_test.sh
+++ tests/remote_test.sh
@@ -11,18 +11,18 @@ echo -e "\n\033[38;5;255m\033[48;5;234m\033[1m  S T R E I S A N D  R E M O T E  
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 VAGRANT_FILENAME="Vagrantfile.remotetest"
-GENERATED_DOCS_DIR="$SCRIPT_DIR/../generated-docs"
-mkdir -p $GENERATED_DOCS_DIR
+GENERATED_DOCS_DIR="${SCRIPT_DIR}/../generated-docs"
+mkdir -p "${GENERATED_DOCS_DIR}"
 
-VAGRANTFILE="$SCRIPT_DIR/../$VAGRANT_FILENAME"
+VAGRANTFILE="${SCRIPT_DIR}/../${VAGRANT_FILENAME}"
 
 function backup_vagrantfile() {
-  cp $VAGRANTFILE $VAGRANTFILE.dist
+  cp "${VAGRANTFILE}" "${VAGRANTFILE}.dist"
 }
 
 function restore_vagrantfile() {
-  cp $VAGRANTFILE.dist $VAGRANTFILE
-  rm $VAGRANTFILE.dist
+  cp "${VAGRANTFILE}.dist" "${VAGRANTFILE}"
+  rm "${VAGRANTFILE}.dist"
 }
 trap restore_vagrantfile EXIT
 
@@ -30,22 +30,22 @@ trap restore_vagrantfile EXIT
 # Vagrantfile.remotetest with the response from a user prompt
 function set_remote_ip() {
   read -r -p "What is the Streisand server IP? " SERVER_IP
-  sed "s/\"REMOTE_IP_HERE\",/\"$SERVER_IP\",/" "$VAGRANTFILE" > "$VAGRANTFILE.new"
-  mv "$VAGRANTFILE.new" "$VAGRANTFILE"
+  sed "s/\"REMOTE_IP_HERE\",/\"${SERVER_IP}\",/" "${VAGRANTFILE}" > "${VAGRANTFILE}.new"
+  mv "${VAGRANTFILE}.new" "${VAGRANTFILE}"
 }
 
 function set_gateway_pass() {
-  local GATEWAY_PASS_FILE="$GENERATED_DOCS_DIR/gateway-password.txt"
+  local GATEWAY_PASS_FILE="${GENERATED_DOCS_DIR}/gateway-password.txt"
 
   read -r -p "What is the Streisand gateway password? " GATEWAY_PASS
 
-  echo "$GATEWAY_PASS" > $GATEWAY_PASS_FILE
+  echo "${GATEWAY_PASS}" > "${GATEWAY_PASS_FILE}"
 }
 
 backup_vagrantfile
 set_remote_ip
 set_gateway_pass
 
-pushd "$SCRIPT_DIR/.."
-  VAGRANT_VAGRANTFILE=$VAGRANT_FILENAME vagrant up --provision
+pushd "${SCRIPT_DIR}/.."
+  VAGRANT_VAGRANTFILE=${VAGRANT_FILENAME} vagrant up --provision
 popd
