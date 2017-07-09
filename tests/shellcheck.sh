be5b2e64704fbb77d93d8366d286456b2aa7d0b3
Daniel <daniel@binaryparadox.net>
2017-07-09 10:37:24 -0400

    Explicitly check the `streisand` wrapper as well.
    It is the one exception to our `.sh` filename convention.

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -15,13 +15,20 @@ if ! command -v shellcheck > /dev/null 2>&1; then
     exit 1
 fi
 
+# NOTE(@cpu): We use -x to follow `source` directives across files
+SHELLCHECK_ARGS="-x"
+
 # Determine the absolute path of this script file's directory
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+# The Streisand project directory is one up from this script's directory, tests/
+PROJECT_DIR="$SCRIPT_DIR/.."
 
-# Move backwards into the Streisand project directory from tests/ subdir
-pushd "$SCRIPT_DIR/.."
+pushd "$PROJECT_DIR"
   # Run shellcheck against all of the `.sh` script files in the Streisand
   # project directory.
-  # NOTE(@cpu): We use -x to follow `source` directives across files
-  find ./ -name '*.sh' -exec shellcheck -x {} \;
+  find ./ -name '*.sh' -exec shellcheck $SHELLCHECK_ARGS {} \;
+
+  # Also explicitly run `shellcheck` against the streisand wrapper script since
+  # it doesn't end in .sh
+  shellcheck $SHELLCHECK_ARGS "$PROJECT_DIR/streisand"
 popd
