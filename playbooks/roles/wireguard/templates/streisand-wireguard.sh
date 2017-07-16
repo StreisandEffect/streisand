e2c98351284ff98852599e74d5f3d28b098e9991
Daniel <daniel@binaryparadox.net>
2017-07-16 09:07:05 -0400

    Jlund review feedback

--- playbooks/roles/wireguard/templates/streisand-wireguard.sh
+++ playbooks/roles/wireguard/templates/streisand-wireguard.sh
@@ -5,7 +5,7 @@
 # Required-Stop:     $network $remote_fs $local_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Persist Wireguard firewall rules for Streisand
+# Short-Description: Persist WireGuard firewall rules for Streisand
 ### END INIT INFO
 
 {% for rule in wireguard_firewall_rules %}
