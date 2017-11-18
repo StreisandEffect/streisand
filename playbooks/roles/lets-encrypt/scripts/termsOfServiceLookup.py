f7699a543b71f065b2ac0ee4735e3ef29eaea1ff
Daniel <daniel@binaryparadox.net>
2017-11-18 11:36:20 -0500

    Pythons love snake_case.

--- playbooks/roles/lets-encrypt/scripts/termsOfServiceLookup.py
+++ playbooks/roles/lets-encrypt/scripts/termsOfServiceLookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-termsOfServiceLookup.py fetches the terms-of-service URL from an ACME
+terms-of-service-lookup.py fetches the terms-of-service URL from an ACME
 /directory URL. This can be used to avoid hardcoding the current
 terms-of-service URL somewhere and having to update it every time
 Let's Encrypt migrates to an updated ToS.
@@ -14,33 +14,33 @@ import urllib2
 import json
 
 
-def errorPrint(*args, **kwargs):
+def error_print(*args, **kwargs):
     """
-    errorPrint prints to stderr and exits the script with a non-zero status
+    error_print prints to stderr and exits the script with a non-zero status
     """
     print(*args, file=sys.stderr, **kwargs)
     sys.exit(1)
 
 
-def fetchDirectory(directoryURL):
+def fetch_directory(directory_URL):
     """
-    fetchDirectory returns the JSON response from an ACME /directory URL
+    fetch_directory returns the JSON response from an ACME /directory URL
     provided. If there is any kind of exception doing so the error is
     printed to stderr and the script terminates with a non-zero exit code.
     """
     # HTTP GET the /directory URL
     try:
-        resp = urllib2.urlopen(directoryURL)
-        directoryJSON = resp.read()
+        resp = urllib2.urlopen(directory_URL)
+        directory_JSON = resp.read()
     except Exception as e:
-        errorPrint("Unable to fetch ACME directory from '{0}': {1}"
-                   .format(directoryURL, e))
-    return directoryJSON
+        error_print("Unable to fetch ACME directory from '{0}': {1}"
+                    .format(directory_URL, e))
+    return directory_JSON
 
 
-def getTOSURL(directoryJSON):
+def get_TOS_URL(directory_JSON):
     """
-    getTOSURL returns the "terms-of-service" key from the /directory JSON's
+    get_TOS_URL returns the "terms-of-service" key from the /directory JSON's
     "meta" key. If there is no "meta" key or no "terms-of-service" key then ""
     is returned. If there is any kind of exception unmarshalling the directory
     JSON the error is printed to stderr and the script terminates with
@@ -48,19 +48,19 @@ def getTOSURL(directoryJSON):
     """
     # Unmarshal the /directory JSON to a dict
     try:
-        directory = json.loads(directoryJSON)
+        directory = json.loads(directory_JSON)
     except Exception as e:
-        errorPrint("Unable to unmarshal ACME directory JSON: {0}"
-                   .format(e))
+        error_print("Unable to unmarshal ACME directory JSON: {0}"
+                    .format(e))
     return directory.get("meta", {}).get("terms-of-service", "")
 
 
-def getTOS(directoryURL):
+def get_TOS(directory_URL):
     """
-    getTOS prints the terms-of-service URL found in the ACME directory JSON
+    get_TOS prints the terms-of-service URL found in the ACME directory JSON
     located at the provided directoryURL to stdout.
     """
-    print(getTOSURL(fetchDirectory(directoryURL)))
+    print(get_TOS_URL(fetch_directory(directory_URL)))
 
 
 if __name__ == "__main__":
@@ -68,4 +68,4 @@ if __name__ == "__main__":
         print("Usage: {0} [ACME /directory URL]".format(sys.argv[0]))
         sys.exit(1)
 
-    getTOS(sys.argv[1])
+    get_TOS(sys.argv[1])
